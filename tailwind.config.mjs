84279221be157f9aa6416c645b9d51a5a421cd9b
Gabo <gabrielriveroeti@gmail.com>
2023-12-28 11:52:59 -0500

    Cacharra en la customizacion de los colores

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,38 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require("tailwindcss/colors");
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: colors.indigo,
+				info: colors.sky,
+				success: colors.teal,
+				warning: colors.amber,
+				danger: colors.rose,
+				muted: {
+				...colors.slate,
+				1000: "#0a101f",
+				},
+				slate: {
+				1000: "#0a101f",
+				},
+				gray: {
+				1000: "#080c14",
+				},
+				zinc: {
+				1000: "#101012",
+				},
+				neutral: {
+				1000: "#080808",
+				},
+				stone: {
+				1000: "#0f0d0c",
+				},
+			}
+		},
 	},
 	plugins: [],
 }
